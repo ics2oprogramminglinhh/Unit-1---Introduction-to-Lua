67664a3ab46007e30639f9a72c7f046902750b0b
s272936 <46288798+s272936@users.noreply.github.com>
2019-02-15 09:14:23 -0500

    Update main.lua

--- HelloWorldLinhH/main.lua
+++ HelloWorldLinhH/main.lua
@@ -1,35 +1,50 @@
 -----------------------------------------------------------------------------------------
 -- Name: Linh Ho
 -- Course: ICS2O
--- This program displays Hello, World on the iPad simulator and "Helloooooo!" to the command
--- terminal.
+-- This program displays Hello, Linh Ho on the iPad simulator and "This is awesome!" 
+-- to the command terminal.
 -----------------------------------------------------------------------------------------
 
 -- print my name to the console
 print ("Hello, Linh Ho")
 
---hide the status bar
+--Hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
---sets the background colour
-display.setDefault("background", 150/255, 50/255, 250/255)
+-- Sets background colour
+display.setDefault("background", 204/255, 255/255, 229/255)
 
--- create a local variable
+-----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES 
+-----------------------------------------------------------------------------------------
+
 local textObject
+local textObjectSignature
+local textObjectNew
 
--- displays text on the screen at position x = 350 and y = 350 with the default font size of 50
-textObject = display.newText( "Hello, Linh Ho!", 350, 350, nil, 50 )
+-----------------------------------------------------------------------------------------
+-- TEXT OBJECTS
+-----------------------------------------------------------------------------------------
 
--- anchor the text object from the top left corner
+-- Displays text on the screen at position x = 350 and y = 350 with the default font size of 50
+textObject = display.newText("Hello, Linh Ho!", 350, 350, nil, 50)
+
+-- Anchor the text object from the top left corner
 textObject.anchorX = 0
 textObject.anchorY = 0
 
--- sets the color of the text object
-textObject:setTextColor (255/255, 0/255, 255/255)
-
--- create another text variable
-local textObjectSignature
+-- Sets color of the text object
+textObject:setTextColor (153/255, 153/255, 255/255)
 
 -- displays text on the screen at the position in the center
--- x = center y= center with the default size of 30
-textObjectSignature = display.newText ("By Linh Ho", display.contentCenterX, display.contentCenterX, nil, 30)
+-- x = center, y= center with default size of 40
+textObjectSignature = display.newText ("By Linh Ho", display.contentCenterX, display.contentCenterX, nil, 40)
+
+-- sets color of the text object 
+textObjectSignature:setTextColor(0/255, 0/255, 52/255)
+
+-- displays "This is awesome!" above the text "Hello, Linh Ho"
+textObjectNew = display.newText("This is awesome!", 500, 250, nil, 80)
+
+-- sets color of the text object
+textObjectNew:setTextColor(255/255, 102/255, 102/255)
